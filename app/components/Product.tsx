9c5f64cb548d1d334e63bd29f944ac272af4b17f
Manuel Atance <atance3@hotmail.com>
2024-02-02 18:20:10 +0100

    Product types and utils done

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image"
+import formatPrice from "@/utils/PriceFormat"
+import { ProductType } from "@/types/ProductType"
 
-export default function Product({name, image, price}: {name: string, image: string, price: number}) {
+export default function Product({name, image, price}: ProductType) {
   return (
     <div>
       <Image src={image} alt={name} width={200} height={200} />
       <h1>{name}</h1>
-      {price}
+      {formatPrice(price as number)}
     </div>
   )
 }
